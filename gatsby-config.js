9c67f1230bf509e24338fa4085eb7bc00ba100d8
redallen <zallen@redhat.com>
2019-04-12 12:10:19 -0400

    loader up

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,14 @@ const ignore = [
   /.*react-docs.*/,
   /.*react-integration.*/,
   `**/\..*`, // dotfiles
-  `**/*.d.ts`,
-  `**/*.test.*`,
-  `**/index.*`,
-  `**/tsconfig.*`,
-  `**/tslint.*`,
-  `**/README.*`,
-  `**/CHANGELOG.*`,
+  `**/*\.d\.ts`,
+  `**/*\.test\.*`,
+  `**/index.ts`,
+  `**/tsconfig*`,
+  `**/tslint*`,
+  `**/README*`,
+  `**/CHANGELOG*`,
+  /.*\.mdx?/, // md files in patternfly-next
 ];
 
 module.exports = {
@@ -33,22 +34,23 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `markdown-pages`,
+        name: `mdx-pages`,
         path: `${__dirname}/src/content`
       }
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `handlebars`,
-    //     path: `${__dirname}/deps/patternfly-next/src/patternfly`,
-    //     ignore: ignore
-    //   }
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `handlebars`,
+        path: `${__dirname}/deps/patternfly-next/src/patternfly/demos/AboutModal`,
+        ignore: ignore
+      }
+    },
     {
       resolve: `gatsby-mdx`,
       options: {
         extensions: ['.md', '.mdx'],
+        // Temporarily disabled because is broken
         // gatsbyRemarkPlugins: [
         //   {
         //     resolve: `gatsby-remark-images`,
@@ -61,27 +63,27 @@ module.exports = {
     },
     // 'gatsby-plugin-catch-links', // catch links in markdown files and use gatsby-link to navigate
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-offline', // this plugin enables Progressive Web App + Offline functionality https://gatsby.app/offline
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'PatternFly',
-        short_name: 'PatternFly',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui'
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://v2.patternfly.org',
-        sitemap: 'https://v2.patternfly.org/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
+    // 'gatsby-plugin-sitemap',
+    // 'gatsby-plugin-offline', // this plugin enables Progressive Web App + Offline functionality https://gatsby.app/offline
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'PatternFly',
+    //     short_name: 'PatternFly',
+    //     start_url: '/',
+    //     background_color: '#663399',
+    //     theme_color: '#663399',
+    //     display: 'minimal-ui'
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-robots-txt',
+    //   options: {
+    //     host: 'https://v2.patternfly.org',
+    //     sitemap: 'https://v2.patternfly.org/sitemap.xml',
+    //     policy: [{ userAgent: '*', allow: '/' }]
+    //   }
+    // },
     // 'gatsby-plugin-sharp',
     // 'gatsby-transformer-sharp',
   ],
--- gatsby-node.js
+++ gatsby-node.js
@@ -3,28 +3,34 @@ const path = require("path");
 exports.createPages = ({graphql, actions}) => {
   const templatePath = path.resolve('./src/templates/mdxTemplate.js');
   const sitemapData = graphql(`
-    {
-      allMdx {
-        edges {
-          node {
-            fileAbsolutePath
-            frontmatter {
-              linkText
-              path
-            }
+  query SiteMap {
+    allMdx {
+      edges {
+        node {
+          fileAbsolutePath
+          frontmatter {
+            linkText
+            path
           }
         }
       }
-    }`);
+    }
+    allHBS: allFile(filter: { absolutePath: { glob: "**/patternfly-next/src/**/examples/index.js" } }) {
+      edges {
+        node {
+          absolutePath
+        }
+      }
+    }
+  }`);
 
   return sitemapData.then(({data}) => {
     // console.log('sitemap', data);
     data.allMdx.edges
-      .map(edge => edge.node)
-      .map(node => node.frontmatter)
+      .map(({node}) => node.frontmatter)
       .filter(frontmatter => frontmatter.path)
       .forEach(frontmatter => {
-        console.log('creating page', frontmatter.path);
+        console.log('creating mdx page', frontmatter.path);
         actions.createPage({
           path: frontmatter.path,
           component: templatePath,
@@ -33,11 +39,46 @@ exports.createPages = ({graphql, actions}) => {
           }
         });
       });
+    
+    data.allHBS.edges
+      .map(({node}) => node.absolutePath)
+      .filter(absolutePath => absolutePath)
+      .forEach(absolutePath => {
+        const split = absolutePath.split('/');
+        const componenet = split[split.length - 3];
+        const section = split[split.length - 4];
+        const path = `/documentation/core/${section}/${componenet}`;
+
+        console.log('creating hbs page', path);
+        actions.createPage({
+          path: path,
+          component: absolutePath,
+          context: {
+            title: componenet
+          }
+        });
+      });
   });
 };
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions, plugins, getConfig }) => {
   actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.md$/,
+          loader: 'html-loader!markdown-loader'
+        },
+        {
+          test: /\.hbs$/,
+          query: {
+            extensions: '.hbs',
+            helperDirs: path.resolve(__dirname, './deps/patternfly-next/build/helpers')
+          },
+          loader: 'handlebars-loader'
+        }
+      ]
+    },
     resolve: {
       alias: {
         '@src': path.resolve(__dirname, './src'),
