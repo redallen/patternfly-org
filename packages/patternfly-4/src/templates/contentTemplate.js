a26d66fdefda9213e37475957a855c522d923d5b
redallen <zallen@redhat.com>
2019-04-29 10:38:45 -0400

    example changing way element is rendered

--- packages/patternfly-4/src/templates/contentTemplate.js
+++ packages/patternfly-4/src/templates/contentTemplate.js
@@ -14,7 +14,25 @@ import {
 import { PatternFlyThemeProvider } from '@patternfly/react-styled-system';
 import { Location } from '@reach/router';
 import { MDXRenderer } from '../components/mdx-renderer';
+import { MDXProvider } from '@mdx-js/react';
+import AutoLinkHeader from '@content/AutoLinkHeader';
 import './template.scss';
+
+const components = {
+  code: class LiveEditWrapper extends React.Component {
+    render() {
+      return (
+        <LiveEdit scope={this.getScope()} id={'' + liveEditCount++} className={this.props.className}>
+          {this.props.children}
+        </LiveEdit>
+      );
+    }
+  },
+  pre: React.Fragment
+};
+for (let i = 1; i <= 6; i++) {
+  components[`h${i}`] = props => <AutoLinkHeader className='ws-linked-heading' is={`h${i}`} {...props}>{props.children}</AutoLinkHeader>;
+}
 
 const navBuilder = navData => {
   return (
@@ -77,9 +95,11 @@ export default function ContentTemplate ({
       <PageSection className="pageSectionStyles" variant={PageSectionVariants.light}>
         <PatternFlyThemeProvider>
           <TextContent>
-            <MDXRenderer>
-              {mdx.code.body}
-            </MDXRenderer>
+            <MDXProvider components={components}>
+              <MDXRenderer>
+                {mdx.code.body}
+              </MDXRenderer>
+            </MDXProvider>
           </TextContent>
         </PatternFlyThemeProvider>
       </PageSection>
